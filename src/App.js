f2c184b09e0e7ea8825ad746507fddcc0c8bb806
Saul Vega <saul_vr07@hotmail.com>
2024-07-22 18:20:38 -0600

    Finishing Rick N Morty App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import { Badge, Box, Card, CardContent, CardMedia, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from '@mui/material';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+  const [selectedCharacters, setSelectedCharacters] = useState([]);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+
+    if (selectedCharacters.find(c => c.id === value.id)) {
+      return;
+    }
+
+    setSelectedCharacters([...selectedCharacters, value]);
+  }
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character', {
+      params: {
+        count: 20,
+        page: 1,
+      },
+    }).then((json) => json.data)
+      .then(data => setCharacters(data.results))
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ flexGrow: 1 }} padding={10}>
+      <FormControl fullWidth margin="normal">
+        <InputLabel>Personaje</InputLabel>
+        <Select
+          labelId="characters"
+          id="characters"
+          label="Selecciona un personaje"
+          onChange={handleChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {
+            characters.map((character) => (<MenuItem value={character}>{ character.name }</MenuItem>))
+          }
+        </Select>
+      </FormControl>
+      <Grid container spacing={2}>
+        {
+          selectedCharacters.map(({ id, name, image, status, species, gender }) => (
+            <Grid item key={id} xs={12} sm={3} md={3}>
+              <Card sx={{ maxWidth: 345, display: 'flex' }}>
+                <CardMedia
+                  component="img"
+                  alt={name}
+                  sx={{ width: 151 }}
+                  image={image}
+                />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
+                    <Typography gutterBottom variant="h5" component='div'>
+                      { name }
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      <Badge color={status === 'Alive' ? 'success' : 'error'} variant="dot" className='badge' />
+                      <span>{ status }</span>
+                      <span>{ species }</span>
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Genero: { gender }
+                    </Typography>
+                  </CardContent>
+                </Box>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
+    </Box>
   );
 }
 
